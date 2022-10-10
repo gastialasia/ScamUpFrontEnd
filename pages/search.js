67a198cf46ae8af3de2cfd9b65bad40f2053f467
Matías Della Torre <matidellatorre@gmail.com>
2022-10-10 10:25:46 -0300

    Search revamped

--- pages/search.js
+++ pages/search.js
@@ -1,4 +1,10 @@
 import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Button from '@mui/material/Button';
 
 import {
   FormControl,
@@ -6,7 +12,7 @@ import {
   MenuItem,
   FormControlLabel,
   InputLabel,
-  Select,
+  Select
 } from "@mui/material";
 
 function SearchPage() {
@@ -18,24 +24,64 @@ function SearchPage() {
   return (
     <div>
       <h1>Search Page</h1>
-
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">searchType</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={searchType}
-          label="searchType"
-          onChange={handleChange}
+      <p>Expand the items below and enter the required information to search for scams. It's not mandatory to fill all fields</p>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
         >
-          <MenuItem value={'Email'}>Email</MenuItem>
-          <MenuItem value={'Phone'}>Phone</MenuItem>
-          <MenuItem value={'Swift code'}>Swift Code</MenuItem>
-          <MenuItem value={'KYC (Know Your Customer)'}>KYC (Know Your Customer)</MenuItem>
-        </Select>
-      </FormControl>
-
-      <TextField fullWidth label="Search" id="fullWidth" />
+          <Typography>Mail Search</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Enter the email to check its reputation 
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Phone search</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          Enter the phone number to check its reputation 
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Swift Code search</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          Enter the Swift Code (banking code) of the account to check its reputation
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Know your customer (KYC)</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          Upload the personal ID of the person to check if it's tagged as dangerous
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Button variant="contained">Search</Button>
     </div>
   );
 }
