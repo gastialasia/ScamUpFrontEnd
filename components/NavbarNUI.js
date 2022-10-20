86f22cf865a414c1f97b98decaed379486dccae8
gastialasia <galasia@itba.edu.ar>
2022-10-20 17:43:09 -0300

    Update NavbarNUI.js

--- components/NavbarNUI.js
+++ components/NavbarNUI.js
@@ -1,25 +1,42 @@
 import { Navbar, Text, Button, Link } from "@nextui-org/react";
 import { AcmeLogo } from "./acmeLogo";
+import NextLink from 'next/link'
+import { useRouter } from "next/router";
 
 export default function NavbarNUI() {
+    const router = useRouter()
+
+    function goToHome() {
+        router.push("/");
+    }
+
     return (
         <Navbar isBordered={true} variant="sticky">
-            <Navbar.Brand>
-            <AcmeLogo />
-            <Text b color="inherit" hideIn="xs">
-                ACME
-            </Text>
-            </Navbar.Brand>
+                <Button light color="primary" auto onClick={goToHome} ripple={false}>
+                    <Navbar.Brand>
+                    <AcmeLogo />
+                    <Text b color="inherit" hideIn="xs">
+                        ScamUp
+                    </Text>
+                    </Navbar.Brand>
+                </Button>
             <Navbar.Content activeColor={"primary"} hideIn="xs" variant={"highlight"}>
-            <Navbar.Link href="#">Features</Navbar.Link>
-            <Navbar.Link isActive href="#">Customers</Navbar.Link>
-            <Navbar.Link href="#">Pricing</Navbar.Link>
-            <Navbar.Link href="#">Company</Navbar.Link>
+            <NextLink href="/pricing">
+                <Navbar.Link>Pricing</Navbar.Link>
+            </NextLink>
+            <NextLink href="/search">
+                <Navbar.Link>Search</Navbar.Link>
+            </NextLink>
+            <NextLink href="/about">
+                <Navbar.Link>About us</Navbar.Link>
+            </NextLink>
             </Navbar.Content>
             <Navbar.Content>
-            <Navbar.Link color="inherit" href="#">
-                Login
-            </Navbar.Link>
+            <NextLink href="/login">
+                <Navbar.Link color="inherit">
+                    Login
+                </Navbar.Link>
+            </NextLink>
             <Navbar.Item>
                 <Button auto flat as={Link} color={"primary"} href="#">
                 Sign Up
