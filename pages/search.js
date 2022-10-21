4bfdd83e3ceadcba532f3838050a5099dc3e40ac
Matías Della Torre <matidellatorre@gmail.com>
2022-10-21 17:17:54 -0300

    Search hardcoded API

--- pages/search.js
+++ pages/search.js
@@ -16,9 +16,8 @@ function SearchPage() {
 
 	async function handleEmail () {
     try {
-      const res = await fetch(`https://jsonplaceholder.typicode.com/posts/1`);
+      const res = await fetch(`http://localhost:8080/email_verification?mail=svalles@itba.edu.ar`);
       const data = await res.json();
-      console.log(data);
     } catch (err) {
       console.log(err);
     }
@@ -104,4 +103,3 @@ function SearchPage() {
 }
 
 export default SearchPage;
-
