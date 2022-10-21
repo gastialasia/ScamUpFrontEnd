5c872782fd159334a226dbc263e6151d1bf9d54e
gastialasia <galasia@itba.edu.ar>
2022-10-21 00:24:38 -0300

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { Collapse, Text } from "@nextui-org/react";
+import { Collapse, Text, Input, Spacer, Container } from "@nextui-org/react";
 import { Grid } from '@nextui-org/react';
-
 
 function SearchPage() {
 
@@ -14,29 +13,30 @@ function SearchPage() {
   return (
     <div>
       <h2>Search for scammers</h2>
-      <p>Expand the items below and enter the required information of the person you think is a scammer. It's not mandatory to fill all fields.</p>                
-      <Collapse.Group accordion={false} shadow className='mt-10'>
-      <Collapse title="Mail">
-        <Text>
-          Enter the email of the person below
-        </Text>
-      </Collapse>
-      <Collapse title="Phone number">
-        <Text>
-          Enter the phone number of the person below
-        </Text>
-      </Collapse>
-      <Collapse title="Swift Code">
-        <Text>
-        Enter the swift code (international banking code) of the person below
-        </Text>
-      </Collapse>
-      <Collapse title="Personal ID (KYC)">
-        <Text>
-        Enter submit the personal ID to do an exhaustive KYC analysis of the person you think is a scammer
-        </Text>
-      </Collapse>
-  </Collapse.Group>
+      <p>Expand the items below and enter the required information of the person you think is a scammer. It's not mandatory to fill all fields.</p>
+        <Collapse.Group accordion={false} bordered css={{ margin: 50 }}>
+        <Collapse title="Mail" expanded>
+          <Text>
+            Enter the email of the person below
+          </Text>
+          <Input placeholder="Next UI" label='email' onChange={e=>console.log(e.target.value)} />
+        </Collapse>
+        <Collapse title="Phone number" expanded>
+          <Text>
+            Enter the phone number of the person below
+          </Text>
+        </Collapse>
+        <Collapse title="Swift Code" expanded>
+          <Text>
+          Enter the swift code (international banking code) of the person below
+          </Text>
+        </Collapse>
+        <Collapse title="Personal ID (KYC)" expanded>
+          <Text>
+          Enter submit the personal ID to do an exhaustive KYC analysis of the person you think is a scammer
+          </Text>
+        </Collapse>
+    </Collapse.Group>
     </div>
   );
 }
