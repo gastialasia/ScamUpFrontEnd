4972f30b1feae2518e8954d09df5d3ba39d2250c
gastialasia <galasia@itba.edu.ar>
2022-10-21 23:19:59 -0300

    sticky navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { NextUIProvider } from '@nextui-org/react';
+// import NavbarNUI from "../components/NavbarNUI";
 
 function MyApp({ Component, pageProps }) {
   return (
