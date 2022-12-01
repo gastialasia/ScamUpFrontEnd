2424ad3dbb67fe15af59edee412769a67b7fdbf9
Matías Della Torre <matidellatorre@gmail.com>
2022-12-01 12:05:07 -0300

    About page with gallery to add photos

--- components/NavbarNUI.js
+++ components/NavbarNUI.js
@@ -38,10 +38,10 @@ export default function NavbarNUI() {
     const handler = () => setVisible(true);
     const handlerSignUp = () => setVisibleSignUp(true);
 
-    async function LogIn() {
+    async function LogIn () {
         const newUser = new ApiUser(emailLogIn, passLogIn);
         const res = await Api.login(newUser);
-        if (res) {
+        if(res){
             setEnterEffect(true);
         }
     }
@@ -50,7 +50,7 @@ export default function NavbarNUI() {
     const [email, setEmail] = React.useState();
     const [pass, setPass] = React.useState();
 
-    async function SignUp() {
+    async function SignUp () {
         console.log(email, pass);
         const res = await Api.createUser(new ApiUser(email, pass));
     }
@@ -68,7 +68,7 @@ export default function NavbarNUI() {
     const [enterEffect, setEnterEffect] = React.useState(false);
 
     useEffect(() => {
-        if (enterEffect) {
+        if(enterEffect){
             Api.setToken(localStorage.getItem("x-token"));
             Api.setUsername(localStorage.getItem("username"));
             // declare the data fetching function
@@ -98,18 +98,18 @@ export default function NavbarNUI() {
             >
                 <Navbar.Toggle showIn="xs" />
                 <Navbar.Brand>
-                    <Image
-                        width={40}
-                        src="/assets/icon.png"
-                        alt="Scam Up Icon"
-                        objectFit="cover"
-                        css={{ pr: 3 }}
+                    <Image 
+                    width={40}  
+                    src="/assets/icon.png"
+                    alt="Scam Up Icon"
+                    objectFit="cover"
+                    css={{ pr: 3 }}
                     />
-                    <Image
-                        width={75}
-                        src="/assets/logo.png"
-                        alt="Scam Up Logo"
-                        objectFit="cover"
+                    <Image 
+                    width={75}  
+                    src="/assets/logo.png"
+                    alt="Scam Up Logo"
+                    objectFit="cover"
                     />
                 </Navbar.Brand>
             </Button>
@@ -120,74 +120,74 @@ export default function NavbarNUI() {
                 enableCursorHighlight
             >
                 <NextLink href="/pricing">
-                    <Navbar.Link isActive={router.pathname == "/pricing"} >Pricing</Navbar.Link>
+                    <Navbar.Link isActive={router.pathname=="/pricing"} >Pricing</Navbar.Link>
                 </NextLink>
                 <NextLink href="/search">
-                    <Navbar.Link isActive={router.pathname == "/search"} >Search</Navbar.Link>
+                    <Navbar.Link isActive={router.pathname=="/search"} >Search</Navbar.Link>
                 </NextLink>
                 <NextLink href="/about">
-                    <Navbar.Link isActive={router.pathname == "/about"} >About us</Navbar.Link>
+                    <Navbar.Link isActive={router.pathname=="/about"} >About us</Navbar.Link>
                 </NextLink>
             </Navbar.Content>
             <Navbar.Content>
-                {userEmail === '' ? <div>
+                { userEmail === '' ? <div>
                     <Navbar.Content>
-                        <Navbar.Item>
-                            <Button auto light as={Link} color={"primary"} onPress={handler} css={{ px: 0 }}>
-                                Log in
-                            </Button>
-                        </Navbar.Item>
-                        <Navbar.Item>
-                            <Button auto flat as={Link} color={"primary"} onPress={handlerSignUp}>
-                                Sign Up
-                            </Button>
-                        </Navbar.Item>
-                    </Navbar.Content>
+                <Navbar.Item>
+                    <Button auto light as={Link} color={"primary"} onPress={handler} css={{ px: 0 }}>
+                        Log in
+                    </Button>
+                </Navbar.Item>
+                <Navbar.Item>
+                    <Button auto flat as={Link} color={"primary"} onPress={handlerSignUp}>
+                        Sign Up
+                    </Button>
+                </Navbar.Item>
+                </Navbar.Content>
                 </div> : <div>
-                    <Dropdown placement="bottom-left">
-                        <Dropdown.Trigger>
-                            <User
-                                bordered
-                                as="button"
-                                size="md"
-                                color="primary"
-                                name={userEmail.substring(0, userEmail.indexOf('@'))}
-                                src="https://cdn.iconscout.com/icon/free/png-128/avatar-372-456324.png"
-                            />
-                        </Dropdown.Trigger>
-                        <Dropdown.Menu color="primary" aria-label="User Actions" onAction={(key = "logout") => { Api.logout(); setUsername(''); setUserEmail(''); console.log(userEmail) }}>
-                            <Dropdown.Item key="profile" css={{ height: "$18" }}>
-                                <Text b color="inherit" css={{ d: "flex" }}>
-                                    Signed in as
-                                </Text>
-                                <Text b color="inherit" css={{ d: "flex" }}>
-                                    {userEmail}
-                                </Text>
-                            </Dropdown.Item>
-                            <Dropdown.Item key="logout" color="error" withDivider>
-                                Log Out
-                            </Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </div>}
-            </Navbar.Content>
-            <Navbar.Collapse >
-                <Navbar.CollapseItem activeColor="secondary" >
-                    <NextLink href="/pricing">
-                        <Link>{"Pricing"}</Link>
-                    </NextLink>
-                </Navbar.CollapseItem>
-                <Navbar.CollapseItem activeColor="secondary" >
-                    <NextLink href="/search">
-                        <Link>{"Search"}</Link>
-                    </NextLink>
-                </Navbar.CollapseItem>
-                <Navbar.CollapseItem activeColor="secondary" >
-                    <NextLink href="/about">
-                        <Link>{"About us"}</Link>
-                    </NextLink>
-                </Navbar.CollapseItem>
-            </Navbar.Collapse>
+                <Dropdown placement="bottom-left">
+          <Dropdown.Trigger>
+            <User
+              bordered
+              as="button"
+              size="md"
+              color="primary"
+              name={userEmail.substring(0, userEmail.indexOf('@'))}
+              src="https://cdn.iconscout.com/icon/free/png-128/avatar-372-456324.png"
+            />
+          </Dropdown.Trigger>
+          <Dropdown.Menu color="primary" aria-label="User Actions" onAction={(key="logout") => {Api.logout(); setUsername(''); setUserEmail(''); console.log(userEmail)}}>
+            <Dropdown.Item key="profile" css={{ height: "$18" }}>
+              <Text b color="inherit" css={{ d: "flex" }}>
+                Signed in as
+              </Text>
+              <Text b color="inherit" css={{ d: "flex" }}>
+              {userEmail}
+              </Text>
+            </Dropdown.Item>
+            <Dropdown.Item key="logout" color="error" withDivider>
+              Log Out
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+                </div> }
+        </Navbar.Content>
+        <Navbar.Collapse >
+            <Navbar.CollapseItem activeColor="secondary" >
+            <NextLink href="/pricing">
+                <Link>{"Pricing"}</Link>
+            </NextLink>
+            </Navbar.CollapseItem>
+            <Navbar.CollapseItem activeColor="secondary" >
+            <NextLink href="/search">
+                <Link>{"Search"}</Link>
+            </NextLink>
+            </Navbar.CollapseItem>
+            <Navbar.CollapseItem activeColor="secondary" >
+            <NextLink href="/about">
+                <Link>{"About us"}</Link>
+            </NextLink>
+            </Navbar.CollapseItem>
+        </Navbar.Collapse>
 
             <Modal
                 closeButton
@@ -209,7 +209,7 @@ export default function NavbarNUI() {
                         color="primary"
                         size="lg"
                         placeholder="Email"
-                        contentLeft={<Mail fill="currentColor" />}
+                        contentLeft={<Mail fill="currentColor"/>}
                         onChange={(e) => setEmailLogIn(e.target.value)}
                     />
                     <Input.Password
@@ -220,7 +220,7 @@ export default function NavbarNUI() {
                         color="primary"
                         size="lg"
                         placeholder="Password"
-                        contentLeft={<Password fill="currentColor" />}
+                        contentLeft={<Password fill="currentColor"/>}
                         onChange={(e) => setPassLogIn(e.target.value)}
                     />
                     <Row justify="space-between">
@@ -234,7 +234,7 @@ export default function NavbarNUI() {
                     <Button auto flat color="error" onPress={closeHandler}>
                         Close
                     </Button>
-                    <Button auto onPress={() => { LogIn(); closeHandler(); }} >
+                    <Button auto onPress={ () => { LogIn(); closeHandler(); }} >
                         Log in
                     </Button>
                 </Modal.Footer>
@@ -281,7 +281,7 @@ export default function NavbarNUI() {
                     <Button auto flat color="error" onPress={closeHandlerSignUp}>
                         Close
                     </Button>
-                    <Button auto onPress={() => { SignUp(); closeHandlerSignUp(); }} >
+                    <Button auto onPress={ () => { SignUp(); closeHandlerSignUp();} } >
                         Sign up
                     </Button>
                 </Modal.Footer>
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,5 @@
 import { IconScamUp } from "../components/icon";
-import { Grid } from "@nextui-org/react";
+import { Image, Grid, Text } from "@nextui-org/react";
 
 function AboutPage() {
 
@@ -9,8 +9,60 @@ function AboutPage() {
                 <h2 style={{ textAlign: 'center' }}>About us</h2>
                 <p>We are a group of Software Engineering students from Instituto Tecnológico de Buenos Aires (ITBA) and this is our project for this term. </p>
             </Grid.Container>
+            <div style={{ padding: 15 }}>
+                <Grid.Container gap={2} justify="space-around">
+                    <Grid xs={6} md={4}>
+                        <div>
+                            <Image
+                                width={320}
+                                height={180}
+                                src="https://github.com/nextui-org/nextui/blob/next/apps/docs/public/nextui-banner.jpeg?raw=true"
+                                alt="Default Image"
+                                objectFit="cover"
+                            />
+                            <Text>Nombre 1</Text>
+                        </div>
+                    </Grid>
+                    <Grid xs={6} md={4}>
+                        <Image
+                            width={320}
+                            height={180}
+                            src="https://github.com/nextui-org/nextui/blob/next/apps/docs/public/nextui-banner.jpeg?raw=true"
+                            alt="Default Image"
+                            objectFit="cover"
+                        />
+                    </Grid>
+                    <Grid xs={6} md={4}>
+                        <Image
+                            width={320}
+                            height={180}
+                            src="https://github.com/nextui-org/nextui/blob/next/apps/docs/public/nextui-banner.jpeg?raw=true"
+                            alt="Default Image"
+                            objectFit="cover"
+                        />
+                    </Grid>
+                    <Grid xs={6} md={4}>
+                        <Image
+                            width={320}
+                            height={180}
+                            src="https://github.com/nextui-org/nextui/blob/next/apps/docs/public/nextui-banner.jpeg?raw=true"
+                            alt="Default Image"
+                            objectFit="cover"
+                        />
+                    </Grid>
+                    <Grid xs={6} md={4}>
+                        <Image
+                            width={320}
+                            height={180}
+                            src="https://github.com/nextui-org/nextui/blob/next/apps/docs/public/nextui-banner.jpeg?raw=true"
+                            alt="Default Image"
+                            objectFit="cover"
+                        />
+                    </Grid>
+                </Grid.Container>
+            </div>
         </div>
-        
+
     );
 }
 
