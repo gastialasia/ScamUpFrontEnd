086778792945f765b235b90ff1b1ed90b06bb681
Matías Della Torre <matidellatorre@gmail.com>
2022-12-02 22:01:21 -0300

    Pricing cards improvement

--- components/PricingCard.js
+++ components/PricingCard.js
@@ -1,41 +1,45 @@
 import { width } from "@mui/system";
-import { Card, Grid, Link, Text, Badge } from "@nextui-org/react";
+import { Card, Grid, Link, Text, Badge, Button } from "@nextui-org/react";
 
-export default function PrincingCard( {planName, pricePerMonth, features, active }) {
-    return (
-        <div>
-        <Card isHoverable css={{ p: "$6", height: "300px", width: "400px" }}>
-      <Card.Header>
-        <Grid.Container css={{ pl: "$6" }}>
-          <Grid xs={12}>
-            <Text h4 css={{ lineHeight: "$xs" }}>
-            {planName}
+export default function PrincingCard({ planName, pricePerMonth, features, showButton, rightText, rightTextColor }) {
+  return (
+    <div>
+      <Card isHoverable css={{ p: "$6", height: "300px", width: "400px" }}>
+        <Card.Header>
+          <Grid.Container css={{ pl: "$6" }}>
+            <Grid xs={12}>
+              <Text h4 css={{ lineHeight: "$xs" }}>
+                {planName}
+              </Text>
+            </Grid>
+            <Grid xs={12}>
+              <Text css={{ color: "$accents8" }}>{pricePerMonth}</Text>
+            </Grid>
+          </Grid.Container>
+        </Card.Header>
+        <Card.Body css={{ py: "$2" }}>
+          <Text>
+            {features.one}
+          </Text>
+          <Text>
+            {features.two}
+          </Text>
+          <Text>
+            {features.three}
+          </Text>
+        </Card.Body>
+        <Card.Footer>
+          <Grid.Container
+            justify="center"
+            alignContent="center"
+          >
+            {showButton ? (<Button>Purchase plan</Button>) : <div></div>}
+            <Text color={rightTextColor} size={18} >
+              {rightText}
             </Text>
-          </Grid>
-          <Grid xs={12}>
-            <Text css={{ color: "$accents8" }}>{pricePerMonth}</Text>
-          </Grid>
-        </Grid.Container>
-      </Card.Header>
-      <Card.Body css={{ py: "$2" }}>
-        <Text>
-        {features.one}
-        </Text>
-        <Text>
-        {features.two}
-        </Text>
-        <Text>
-        {features.three}
-        </Text>
-      </Card.Body>
-      <Card.Footer>
-        <Text
-          style={ active? { color: 'black' } : {color: 'grey'} }
-        >
-          {active? "Current plan": "Coming soon"}
-        </Text>
-      </Card.Footer>
-    </Card>
-        </div>
-    );
+          </Grid.Container>
+        </Card.Footer>
+      </Card>
+    </div>
+  );
 }
--- pages/pricing.js
+++ pages/pricing.js
@@ -15,13 +15,13 @@ function PricingPage() {
             </Grid.Container>
             <Grid.Container justify="center" alignContent="" gap={4} wrap>
             <Grid>
-                <PrincingCard planName="Free Plan" pricePerMonth="$0/month" features={{one:"✅   10 searches per month", two:"✅   Individual searches only"}} active="true" />
+                <PrincingCard planName="Free Plan" pricePerMonth="$0/month" features={{one:"✅   10 searches per month", two:"✅   Individual searches only"}} rightText="Current plan" rightTextColor="primary" />
             </Grid>
             <Grid>
-                <PrincingCard planName="Pro Plan" pricePerMonth="$10/month" features={{one:"✅   Unlimited searches per month", two:"✅   Search multiple types of information and get a reputation score"}} />
+                <PrincingCard planName="Pro Plan" pricePerMonth="$10/month" features={{one:"✅   Unlimited searches per month", two:"✅   Search multiple types of information and get a reputation score"}} showButton = {true} />
             </Grid>
             <Grid>
-                <PrincingCard planName="Business Plan" pricePerMonth="$50/month" features={{one:"✅   Unlimited searches per month", two:"✅   Search multiple types of information and get a reputation score", three:"✅   Complete access to our API"}} />
+                <PrincingCard planName="Business Plan" pricePerMonth="$50/month" features={{one:"✅   Unlimited searches per month", two:"✅   Search multiple types of information and get a reputation score", three:"✅   Complete access to our API"}} rightText="Coming soon" rightTextColor="grey" />
             </Grid>
             </Grid.Container>
         </div>
