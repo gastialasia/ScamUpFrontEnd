58c59d47ac287b739f815dad6cbf556ae870003a
gastialasia <86937493+gastialasia@users.noreply.github.com>
2022-12-04 18:27:39 -0300

    updated pricing page for xl screens

--- pages/pricing.js
+++ pages/pricing.js
@@ -9,21 +9,24 @@ function PricingPage() {
     const context = useContext(AppContext);
 
     return (
-        <div div style={{ padding: 15 }}>
-            <Col>
-                <Text h2>Pricing</Text>
+        <div div style={{ padding: 12 }}>
+            <Grid.Container justify="center" direction="column" alignContent="center">
+                <h2 style={{ textAlign: 'center' }}>Pricing</h2>
+                <p>This are the available plans</p>
+            </Grid.Container>
+            <div style={{ padding: 15 }}>
                 <Grid.Container justify="center" gap={2} fluid>
-                    <Grid xs={12} sm={6} md={4} lg={4}>
+                    <Grid xs={12} sm={6} md={4} lg={3}>
                         <PrincingCard planName="Free Plan" pricePerMonth="$0/month" features={{one:"✅   10 searches per month", two:"✅   Individual searches only"}} rightText="Current plan" rightTextColor="primary" />
                     </Grid>
-                    <Grid xs={12} sm={6} md={4} lg={4}>
+                    <Grid xs={12} sm={6} md={4} lg={3}>
                         <PrincingCard planName="Pro Plan" pricePerMonth="$10/month" features={{one:"✅   Unlimited searches per month", two:"✅   Search multiple types of information and get a reputation score"}} showButton = {true} />
                     </Grid>
-                    <Grid xs={12} sm={6} md={4} lg={4}>
+                    <Grid xs={12} sm={6} md={4} lg={3}>
                         <PrincingCard planName="Business Plan" pricePerMonth="$50/month" features={{one:"✅   Unlimited searches per month", two:"✅   Search multiple types of information and get a reputation score", three:"✅   Complete access to our API"}} rightText="Coming soon" rightTextColor="grey" />
                     </Grid>
                 </Grid.Container>
-            </Col>
+            </div>
         </div>
     );
 }
